0a2af9a45ce98ee9ff1db68a7290e66a4e561c5f
Sukaina Batool <sukainasbb@gmail.com>
2022-04-03 01:06:01 +0500

    Purana code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+//import Auth from "./components/Auth"
+import Header from './'
+import { DataProvider } from './GlobalState'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <DataProvider>
+            <Router>
+                <div className="App">
+
+
+                </div>
+            </Router>
+        </DataProvider>
+    )
 }
 
-export default App;
+export default App
